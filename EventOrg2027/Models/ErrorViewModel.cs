a2989a9a16351cb011f80413f00ff77c5a99c49b
marcelolopes <marcelosepol96@hotmail.com>
2020-11-01 19:30:24 +0000

    Initial check-in of module EventOrg2027

---
+++ EventOrg2027/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EventOrg2027.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
