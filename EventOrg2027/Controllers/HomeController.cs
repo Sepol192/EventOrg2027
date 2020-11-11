18195179f2eee306247f9f3f942b4715ec788f85
marcelolopes <marcelosepol96@hotmail.com>
2020-11-11 20:58:09 +0000

    Adição da pagina sobre

--- EventOrg2027/Controllers/HomeController.cs
+++ EventOrg2027/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace EventOrg2027.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Sobre()
         {
             return View();
         }
