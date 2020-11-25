9377806558c5936bdda29f1e6cd21ad25d510155
Marcelo Lopes <marcelosepol96@hotmail.com>
2020-11-25 00:07:23 +0000

    Criaçao da Base de Dados Tabela Localidade Tabela Tipos de Eventos

---
+++ EventOrg2027/Models/Localidade.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventOrg2027.Models
+{
+    public class Localidade
+    {
+        [Column("LocalidadeId")]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required]
+        public int LocalidadeId { get; set; }
+
+        [Column("NomeLocalidade")]
+        [Required]
+        [StringLength(50)]
+        public string NomeLocalidade { get; set; }
+    }
+}
---
+++ EventOrg2027/Models/TipoEventos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventOrg2027.Models
+{
+    public class TipoEventos
+    {
+        [Column("TipoEventosId")]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required]
+        public int TipoEventosId { get; set; }
+
+        [Column("NomeTipoEventos")]
+        [Required]
+        [StringLength(50)]
+        public string NomeTipoEventos { get; set; }
+    }
+}
