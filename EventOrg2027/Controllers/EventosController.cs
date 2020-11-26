475ad17ad69b5b73f06bd2b275be821106b48cbe
João Aires <48353968+joaoaires@users.noreply.github.com>
2020-11-26 02:55:54 +0000

    CRUD Eventos

---
+++ EventOrg2027/Controllers/EventosController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EventOrg2027.Data;
+using EventOrg2027.Models;
+
+namespace EventOrg2027.Controllers
+{
+    public class EventosController : Controller
+    {
+        private readonly EventOrgDbContext _context;
+
+        public EventosController(EventOrgDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Eventos
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Eventos.ToListAsync());
+        }
+
+        // GET: Eventos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var eventos = await _context.Eventos
+                .FirstOrDefaultAsync(m => m.EventosId == id);
+            if (eventos == null)
+            {
+                return NotFound();
+            }
+
+            return View(eventos);
+        }
+
+        // GET: Eventos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Eventos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("EventosId,NomeEventos,Descricao,HoraInicio,Lotacao,DataRealizacao")] Eventos eventos)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(eventos);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(eventos);
+        }
+
+        // GET: Eventos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var eventos = await _context.Eventos.FindAsync(id);
+            if (eventos == null)
+            {
+                return NotFound();
+            }
+            return View(eventos);
+        }
+
+        // POST: Eventos/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("EventosId,NomeEventos,Descricao,HoraInicio,Lotacao,DataRealizacao")] Eventos eventos)
+        {
+            if (id != eventos.EventosId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(eventos);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EventosExists(eventos.EventosId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(eventos);
+        }
+
+        // GET: Eventos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var eventos = await _context.Eventos
+                .FirstOrDefaultAsync(m => m.EventosId == id);
+            if (eventos == null)
+            {
+                return NotFound();
+            }
+
+            return View(eventos);
+        }
+
+        // POST: Eventos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var eventos = await _context.Eventos.FindAsync(id);
+            _context.Eventos.Remove(eventos);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EventosExists(int id)
+        {
+            return _context.Eventos.Any(e => e.EventosId == id);
+        }
+    }
+}
